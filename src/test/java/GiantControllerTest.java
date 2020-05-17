20b62d0d277a0649eea8b88b4df7de2fb9eeaf11
Ksenia <oksankapavlishak24@gmail.com>
2020-05-17 12:34:49 +0200

    .

---
+++ src/test/java/GiantControllerTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GiantControllerTest {
+    private  GiantModel giantModel;
+    private GiantView giantView;
+    private GiantController giantController;
+
+    @BeforeEach
+    public  void setUp(){
+        System.out.println("PRZED");
+        giantModel = Mockito.mock(GiantModel.class);
+        giantController = Mockito.mock(GiantController.class);
+        giantView = Mockito.mock(GiantView.class);
+        giantController = new GiantController(giantModel, giantView);
+
+    }
+    @Test
+    public void testSetHealth(){
+       // Mockito.verify(giantModel,
+        //        Mockito.times(2));
+        for( final Health health: Health.values()){
+            giantController.setHealth(health);
+            Mockito.verify(giantModel).setHealth(health);
+        }
+        giantController.getHealth();
+        Mockito.verify(giantModel).getHealth();
+
+    }
+    @Test
+    public void  testSetFatigue(){
+        for( final Fatigue fatigue: Fatigue.values()){
+            giantController.setFatigue(fatigue);
+            Mockito.verify(giantModel).setFatigue(fatigue);
+        }
+        giantController.getFatigue();
+        Mockito.verify(giantModel).getFatigue();
+    }
+    @Test
+    public void testSetNourishment(){
+        for ( final  Nourishment nourishment: Nourishment.values()){
+            giantController.setNourishment(nourishment);
+            Mockito.verify(giantModel).setNourishment(nourishment);
+
+        }
+        giantController.getNourishment();
+        Mockito.verify(giantModel).getNourishment();
+    }
+}
